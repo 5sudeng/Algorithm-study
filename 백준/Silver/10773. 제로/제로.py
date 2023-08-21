a31e6664199eaeefa690ec8d8c5c924c1578ceb0
Sujeong, Oh <89963974+5sudeng@users.noreply.github.com>
2023-08-21 21:09:49 +0900

    [Silver IV] Title: 제로, Time: 100 ms, Memory: 32036 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.py
+++ 백준/Silver/10773. 제로/제로.py
@@ -2,7 +2,7 @@ import sys
 k = int(sys.stdin.readline())
 array = []
 for i in range(k) :
-    x = int(input())
+    x = int(sys.stdin.readline())
     if x == 0 :
         array.pop()
         continue
