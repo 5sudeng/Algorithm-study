e00b24d10bb064ad580072472f7268763a9f1aed
Sujeong, Oh <89963974+5sudeng@users.noreply.github.com>
2023-08-24 14:47:21 +0900

    [Silver IV] Title: ATM, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.py
@@ -0,0 +1,11 @@
+import sys
+n = int(sys.stdin.readline())
+time = list(map(int, sys.stdin.readline().split()))
+
+time.sort()
+result = 0
+
+for i, t in enumerate(time) :
+    result += t*(n-i)
+
+print(result)
