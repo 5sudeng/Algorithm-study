661ed73c88fdb2d4267c1687ed13edccf36654ed
Sujeong, Oh <89963974+5sudeng@users.noreply.github.com>
2023-09-02 12:43:26 +0900

    [Gold V] Title: 토마토, Time: 460 ms, Memory: 159172 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,36 +1,36 @@
 from collections import deque
-m,n = map(int,input().split())
-tomato = []
-for _ in range(n):
-    tomato.append(list(map(int,input().split())))
+
+m, n = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
+
 queue = deque()
-for i in range(n):
-    for j in range(m):
-        if tomato[i][j] == 1:
-            queue.append((i,j))
 
+for i in range(n) :
+    for j in range(m) :
+        if graph[i][j] == 1 :
+            queue.append((i, j))
 
-dx = [-1,0,1,0]
-dy = [0,-1,0,1]
-while queue:
-    x,y = queue.popleft()
-    for i in range(4):
-        nx = x+dx[i]
-        ny = y+dy[i]
-        if 0<=nx<n and 0<=ny<m:
-            if tomato[nx][ny] == -1:
-                continue
-            if tomato[nx][ny] == 0:
-                queue.append((nx,ny))
-                tomato[nx][ny] = tomato[x][y]+1
-                
-flag = False
-for i in range(n):
-    for j in range(m):
-        if tomato[i][j] == 0:
-            flag = True
-            break
-if flag:
+dx = [-1, 1, 0, 0] #상 하 좌 우
+dy = [0, 0, -1, 1]
+
+while queue :
+    x, y = queue.popleft()
+    for i in range(4) :
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if nx < 0 or ny < 0 or nx >= n or ny >= m or graph[nx][ny] == -1 :
+            continue
+        if graph[nx][ny] == 0 :
+            graph[nx][ny] = graph[x][y] + 1
+            queue.append((nx, ny))
+
+isBreak = False
+for i in range(n) :
+    for j in range(m) :
+        if graph[i][j] == 0 :
+            isBreak = True
+
+if isBreak :
     print(-1)
-else:
-    print(max(map(max,tomato))-1)
+else :
+    print(max(map(max, graph)) - 1)
